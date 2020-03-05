5805c3d2d5952eeb6e27fef91e338911799d7c1f
nadine-padernal <60122574+nadine-padernal@users.noreply.github.com>
2020-03-05 10:56:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,27 @@ $module = (isset($_GET['mod']) && $_GET['mod'] != '') ? $_GET['mod'] : '';
 </head>
 <body>
 
+<nav>
+<ul class="primary">
+		<li>
+      <a href="index.php">Home</a>
+    </li>
+    <li>
+      <a href="">Products</a>
+      <ul class="sub">
+				<li><a href='index.php?mod=Prod'>Product List</a></li>
+				<li><a href='index.php?mod=FCrt'>Add Product</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="">Categories</a>
+      <ul class="sub">
+				<li><a href='index.php?mod=Cat'>Category List</a></li>
+      </ul>
+    </li>
+  </ul>
+</nav>
+
   <div id="container">
       <?php
       switch ($module) {
