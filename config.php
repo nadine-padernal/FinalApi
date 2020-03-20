6fbea37def48f0a85df5c0f6eca842f1dbb83bce
nadine-padernal <60122574+nadine-padernal@users.noreply.github.com>
2020-03-20 16:59:25 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$google_client = new Google_Client();
+
+$google_client->setClientId('410146654780-dfpjcnbrs7vpqkm62hs14l7bhtglj9gc.apps.googleusercontent.com');
+
+$google_client->setClientSecret('LpwQD8c_KvD-zYAblbPP8GXz');
+
+$google_client->setRedirectUri('https://apiact.herokuapp.com/index.php');
+
+//
+$google_client->addScope('email');
+
+$google_client->addScope('profile');
+
+
+session_start()
+?>
---
+++ config2.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+
+$facebook = new \Facebook\Facebook([
+  'app_id'      => '673026740130754',
+  'app_secret'     => 'e6a27119004c6d70eed71ac7f59c8b4b',
+  'default_graph_version'  => 'v2.10'
+]);
+
+?>
