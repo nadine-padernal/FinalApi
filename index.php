9e8dbcae3899a621702adf18305b9f1651741609
nadine-padernal <60122574+nadine-padernal@users.noreply.github.com>
2020-03-20 13:51:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -28,14 +28,7 @@ $module = (isset($_GET['mod']) && $_GET['mod'] != '') ? $_GET['mod'] : '';
     </li>
   </ul>
 </nav>
-	
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
+
 	<a href="#" onclick="signOut();">Sign out</a>
 <script>
   function signOut() {
